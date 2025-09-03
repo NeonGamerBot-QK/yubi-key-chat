91b15d8fece2c5c3b3b2265ceabac22f5a1d513b
Saahil <neon@saahild.com>
2025-09-02 23:50:18 -0400

    todays new work ig.

---
+++ test_yubi.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const yubi = require('yub')
+
+yubi.init(process.env.YUBI_ID, process.env.YUBI_SECRET)
+
+yubi.verify('cccccbceufvlunbgnbhgccnbdfjidtbbetjrgfnetfif', (e, d) => {
+    console.log(e, d)
+})
+
---
+++ todo.md
@@ -0,0 +1,10 @@
+ok neon what are we doing here
+so 
+its a basic socket io chat app
+w/ rooms
+but
+yubikey otp login
+which u check via api
+and create an account via serial number
+usernames are mid
+only nicknames
