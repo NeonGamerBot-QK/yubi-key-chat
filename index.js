6dc9522fc01203349d1ad899c59021d6f77ce563
Saahil <neon@saahild.com>
2025-09-06 01:12:11 -0400

    todays work

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ require("dotenv/config");
 const express = require("express");
 const { verifyYubiKey } = require("./utils");
 const Prisma = require("@prisma/client").PrismaClient;
+var cookieParser = require('cookie-parser')
+const socketIo = require('socket.io')
+const http = require('http')
 const prisma = new Prisma();
 const app = express();
 const port = 3000;
+const server = http.createServer(app);
+const io = socketIo(server);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 app.get("/", (req, res) => {
   // res.send("change to rendered");
   // res.render('index');
@@ -23,13 +29,16 @@ app.get("/chat", (req, res) => {
   if (!req.cookies.serial) {
     return res.redirect("/login");
   }
+  // res.send('chat')
+  res.render("layout", { title: "Chat", file: "index.ejs" });
+
 });
 
 app.get("/login", (req, res) => {
   if (req.cookies.serial) {
     return res.redirect("/chat");
   }
-  res.render("index");
+  res.render("layout", { title: "Login", file: "login.ejs" });
 });
 
 app.post("/login", async (req, res) => {
@@ -38,10 +47,11 @@ app.post("/login", async (req, res) => {
   console.log(otp);
   try {
     const yubData = await verifyYubiKey(otp);
-    console.log(yubData);
+    // console.log(yubData);
     if (!yubData.valid) {
       return res.status(400).json({ message: "Invalid OTP" });
     }
+    yubData.serial = yubData.serial.toString()
     const userData = await prisma.user.findFirst({
       where: {
         serial: yubData.serial,
@@ -56,11 +66,13 @@ app.post("/login", async (req, res) => {
           serial: yubData.serial,
           lastOtp: yubData.otp,
           yubiKeyOtps: [yubData],
+          identityString: yubData.identity
           // add other fields as necessary
         },
       });
       // set cookies to be like the login serial idk
-      res.cookies.serial = yubData.serial;
+      // req.cookies.serial = yubData.serial;
+      res.cookie('serial', yubData.serial, { maxAge: 900000, httpOnly: true });
       // redirect to dashboard
       res.redirect("/chat");
     } else {
@@ -79,17 +91,32 @@ app.post("/login", async (req, res) => {
         },
       });
 
-      res.cookies.serial = yubData.serial;
+      res.cookie('serial', yubData.serial, { maxAge: 900000, httpOnly: true });
       // redirect to dashboard
       res.redirect("/chat");
     }
   } catch (e) {
     console.error(e);
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ message: e.message, error: true });
   }
 });
-
+const users = []
+const last_10_msgs = []
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  users.push(socket.id)
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    const index = users.indexOf(socket.id)
+    if (index > -1) {
+      users.splice(index, 1);
+    }
+  });
+  socket.on('chat message', (msg, author) => {
+    io.emit('chat message', msg, author);
+  });
+})
 // verifyYubiKey
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
